98d553990d2b46f9b2bc8d51947504f7e37e8b31
Mauro456 <mauro.fi@gmail.com>
2024-03-26 10:21:28 +0200

    Add project files.

---
+++ Harjoitus1_Ajoneuvo/Ajoneuvo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus1_Ajoneuvo
+{
+    internal class Ajoneuvo
+    {
+
+    }
+}
