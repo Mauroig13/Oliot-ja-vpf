584171f3a19c7092eb3f71f150e4d1a53c76af3e
Mauro456 <mauro.fi@gmail.com>
2024-03-26 10:53:25 +0200

    Harjoitus1_Ajoneuvo commit

--- Harjoitus1_Ajoneuvo/Ajoneuvo.cs
+++ Harjoitus1_Ajoneuvo/Ajoneuvo.cs
@@ -8,6 +8,22 @@ namespace Harjoitus1_Ajoneuvo
 {
     internal class Ajoneuvo
     {
+        public string Nimi {  get; set; }
+        public int Nopeus { get; set; }
+        public int Renkaat { get; set; }
 
+        public void TulostaData()
+        {
+            Console.WriteLine("Ajoneuvo: ");
+            Console.WriteLine("-- Nimi: " + Nimi);
+            Console.WriteLine("-- Nopeus: " + Nopeus);
+            Console.WriteLine("-- Renkaat: " + Renkaat);
+        }
+
+        public string ToString()
+        { 
+            string merkkijono = "Ajoneuvo: " + Nimi + ".Nopeus: " + Nopeus + "km/h. Renkaat: " + Renkaat + "kpl";
+            return merkkijono;
+        }
     }
 }
--- Harjoitus1_Ajoneuvo/Program.cs
+++ Harjoitus1_Ajoneuvo/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Harjoitus1_Ajoneuvo;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Olio Ajoneuvo -luokasta
+        Ajoneuvo auto = new Ajoneuvo();
+
+        //Asetetaan oliolle "auto" arvot
+        auto.Nimi = "Toyota";
+        auto.Nopeus = 120;
+        auto.Renkaat = 4;
+
+        //Käytetään "auto" olion toimintoja
+        auto.TulostaData();
+
+        string autonTiedot = auto.ToString();
+        
+        Console.WriteLine(autonTiedot);
+
+        //Tyhjä rivi
+        Console.WriteLine();
+
+        Ajoneuvo moottoripyora = new Ajoneuvo();
+        moottoripyora.Nimi = "BMW s1000rr";
+        moottoripyora.Nopeus = 310;
+        moottoripyora.Renkaat = 2;
+
+        moottoripyora.TulostaData();
+        
+        string moottoripyoraTiedot= moottoripyora.ToString();
+        Console.WriteLine(moottoripyoraTiedot);
+
+
+
+    }
+}
