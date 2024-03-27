bbd2513f920ce9472fcf5a9e6647ac95ff5acb01
Mauro456 <mauro.fi@gmail.com>
2024-03-27 14:46:14 +0200

    Harjoitus3_Kiaus Commit

---
+++ Harjoitus3_Kiaus/Kiuas.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Harjoitus3_Kiaus
+{
+    class Kiuas
+    {
+        private bool päällä;
+        private int lämpötila;
+        private int kosteus;
+
+        // Konstruktori Kiuas-luokalle
+        public Kiuas()
+        {
+            päällä = false;
+            lämpötila = 0;
+            kosteus = 0;
+        }
+
+        // Metodi kiuas päälle
+        public void Käynnistä()
+        {
+            päällä = true;
+            Console.WriteLine("Kiuas on nyt päällä.");
+        }
+
+        // Metodi kiuas pois päältä
+        public void Sammuta()
+        {
+            päällä = false;
+            Console.WriteLine("Kiuas on nyt pois päältä.");
+        }
+
+        // Metodi lämpötilan säätöön
+        public void SäädäLämpötila(int uusiLämpötila)
+        {
+            lämpötila = uusiLämpötila;
+            Console.WriteLine("Lämpötila asetettu: " + lämpötila + " astetta.");
+        }
+
+        // Metodi kosteuden säätöön
+        public void SäädäKosteus(int uusiKosteus)
+        {
+            kosteus = uusiKosteus;
+            Console.WriteLine("Kosteus asetettu: " + kosteus + "%.");
+        }
+
+        // Metodi tulostamaan kiuas tiedot
+        public void TulostaTiedot()
+        {
+            Console.WriteLine("Kiuas päällä: " + (päällä ? "Kyllä" : "Ei"));
+            Console.WriteLine("Lämpötila: " + lämpötila + " astetta");
+            Console.WriteLine("Kosteus: " + kosteus + "%");
+        }
+    }
+}
+
---
+++ Harjoitus3_Kiaus/Program.cs
@@ -0,0 +1,19 @@
+﻿using Harjoitus3_Kiaus;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Luodaan uusi kiuas-olio
+        Kiuas kiuas = new Kiuas();
+
+        // Käynnistetään kiuas, säädellään lämpötilaa ja kosteutta, tulostetaan tiedot
+        kiuas.Käynnistä();
+        kiuas.SäädäLämpötila(70);
+        kiuas.SäädäKosteus(50);
+        kiuas.TulostaTiedot();
+
+        // Sammutetaan kiuas
+        kiuas.Sammuta();
+    }
+}
