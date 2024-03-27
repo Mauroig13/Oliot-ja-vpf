c95315f37fc0131b2d3a2a51872ad610a064eedd
Mauro456 <mauro.fi@gmail.com>
2024-03-27 14:57:27 +0200

    Harjoitus4_Albumi Commit

---
+++ Harjoitus4_Albumi/Albumi.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Luodaan Kappale-luokka
+class Kappale
+{
+    public string Nimi { get; set; }
+    public string Artisti { get; set; }
+    public double Kesto { get; set; } 
+
+    // Konstruktori
+    public Kappale(string nimi, string artisti, double kesto)
+    {
+        Nimi = nimi;
+        Artisti = artisti;
+        Kesto = kesto;
+    }
+
+    // Override ToString method to print song details
+    public override string ToString()
+    {
+        return $"Kappale: {Nimi} - Artisti: {Artisti} - Kesto: {Kesto} sekuntia";
+    }
+}
+
+// Luodaan Albumi-luokka, joka sisältää kappaleita
+class Albumi
+{
+    public string Nimi { get; set; }
+    public string Artisti { get; set; }
+    public List<Kappale> Kappaleet { get; set; }
+
+    // Konstruktori
+    public Albumi(string nimi, string artisti)
+    {
+        Nimi = nimi;
+        Artisti = artisti;
+        Kappaleet = new List<Kappale>(); // Alustetaan lista kappaleille
+    }
+
+    // Lisää kappale albumiin
+    public void LisääKappale(Kappale kappale)
+    {
+        Kappaleet.Add(kappale);
+    }
+
+    // Tulosta albumin tiedot
+    public void TulostaAlbuminTiedot()
+    {
+        Console.WriteLine($"Albumi: {Nimi} - Artisti: {Artisti}");
+        Console.WriteLine("Kappaleet:");
+        foreach (var kappale in Kappaleet)
+        {
+            Console.WriteLine(kappale);
+        }
+    }
+}
---
+++ Harjoitus4_Albumi/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Luodaan kappaleita
+        Kappale kappale1 = new Kappale("Rahh", "MSG", 240);
+        Kappale kappale2 = new Kappale("Dahh", "MSG", 180);
+        Kappale kappale3 = new Kappale("Bahh", "MSG", 320);
+
+        // Luodaan albumi ja lisätään siihen kappaleita
+        Albumi albumi = new Albumi("Albumi Loco", "Mauro Sanchez Gonzalez");
+        albumi.LisääKappale(kappale1);
+        albumi.LisääKappale(kappale2);
+        albumi.LisääKappale(kappale3);
+
+        // Tulostetaan albumin tiedot
+        albumi.TulostaAlbuminTiedot();
+    }
+}
